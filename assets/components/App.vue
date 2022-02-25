1ba3b1b5df31c4d375b115a4d20cb43c629b1d62
Eduard Costea <costeaeduardnic@gmail.com>
2022-02-25 14:28:48 +0100

    initial commit with encore

---
+++ assets/components/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2 class="center">My Application</h2>
+        <div v-text="message"></div>
+        <ul>
+            <li :key="country.id" v-for="country in countries">{{ country }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "A list of words",
+            countries: [],
+
+        };
+    },
+    mounted() {
+        let el = document.querySelector("div[data-words]");
+        let myCountries = el.dataset.words.split(",");
+
+        this.countries.push.apply(this.countries, myCountries);
+    }
+};
+
+
+</script>
+
+<style>
+
+</style>
